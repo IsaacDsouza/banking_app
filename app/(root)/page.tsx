e2c79a769553e770f50976bb9e82a31bf29e4ef2
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-12 11:56:05 +0530

    HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
+import HeaderBox from '@/components/HeaderBox';
 import React from 'react';
 
 
 
 const Home = () => {
+
+    const loggedIn = { firstName : 'Isaac'};
+
     return (
-        <div>
-            Home
+        <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type = "greeting"
+                    title = "Welcome"
+                    user = {loggedIn?.firstName || 'Guest'}
+                    subtext = "Access and manage your account and transactions efficiently."
+                />
+            </header>
         </div>
+        </section>
     );
 };
 
